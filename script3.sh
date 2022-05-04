7f422c43ca35acee681ac70945697b4c7512095d
maxsimka234 <max.borejchuk@gmail.com>
2022-05-04 20:39:39 +0000

    script3

---
+++ script3.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+file="script3.txt"
+
+echo -e "---------uptime------ \n">$file
+uptime>>$file
+echo -e "\n---------------------- \n\n">>$file
+
+echo -e "---------users------ \n">>$file
+users>>$file
+echo -e "\n---------------------- \n\n">>$file
+
+echo -e "---------recent logins------ \n">>$file
+last>>$file
+echo -e "\n---------------------- \n\n">>$file
+
+echo -e "---------top 10 process by RAM usage---------">>$file
+ps aux --sort -rss | head >>$file
+echo -e "\n---------------------------------------- \n\n">>$file
+
+echo -e "---------all open TCP/UDP ports-----------">>$file
+ss -l >>$file
+echo -e "\n---------------------------------------- \n\n">>$file
+
+echo -e "---------Virtual memoty usage-----------">>$file
+vmstat >>$file
+echo -e "\n---------------------------------------- \n\n">>$file
+
+echo -e "---------usage of disck space-----------">>$file
+df -h >>$file
+echo -e "\n---------------------------------------- \n\n">>$file
+
